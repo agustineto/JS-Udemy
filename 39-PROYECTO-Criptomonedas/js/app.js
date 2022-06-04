c0b4da8a44991682e7378f42f71f0c9070bc0a7f
agustin Zavala Totozintle <agustinzavalat@gmail.com>
2022-06-04 16:02:08 -0500

    last changes

--- 39-PROYECTO-Criptomonedas/js/app.js
+++ 39-PROYECTO-Criptomonedas/js/app.js
@@ -1,12 +1,31 @@
 const selectMoneda = document.querySelector("#moneda")
 const selectCryptos = document.querySelector('#criptomonedas')
+const formulario = document.querySelector('#formulario')
+
+const objBusqueda = {
+    moneda: '',
+    criptomoneda: ''
+}
 
 window.addEventListener('load', ()=> {
-    console.log("cargo...")
     APIMonedas()
     consultarCryptomonedas()
+    selectCryptos.addEventListener('change', leerBusqueda)
+    selectMoneda.addEventListener('change', leerBusqueda)
+    formulario.addEventListener('submit', submitFormulario)
 })
 
+function leerBusqueda(e){
+    objBusqueda[e.target.name] = e.target.value
+    objBusqueda[e.target.name] = e.target.value
+    console.log(objBusqueda)
+
+}
+
+// CREAR UN PROMISE
+const obtenerCryptos = cryptomonedas => new Promise ( resolve => {
+    resolve(cryptomonedas)
+})
 
 function APIMonedas(){
 
@@ -14,7 +33,6 @@ function APIMonedas(){
     fetch(url)
         .then(respuesta => respuesta.json())
         .then(datos => {
-            console.log(datos)
             datos.forEach( elementos => {
                 const option = document.createElement('option')
                 option.value = elementos.val
@@ -23,11 +41,29 @@ function APIMonedas(){
             });
            
         })
+    
 }
 
 function consultarCryptomonedas(){
     const url = "https://min-api.cryptocompare.com/data/top/totalvolfull?limit=10&tsym=USD"
     fetch(url)
         .then(respuesta => respuesta.json())
-        .then(datos => )
+        .then(datos => obtenerCryptos(datos.Data) )
+        .then( cryptomonedas => llenaSelectCryptos(cryptomonedas)) 
+}
+
+function llenaSelectCryptos(cryptomonedas){
+
+    cryptomonedas.forEach( crypto => {
+        const {FullName, Name } = crypto.CoinInfo;
+        const option = document.createElement('option')
+        option.value = Name
+        option.textContent = FullName
+
+        selectCryptos.appendChild(option)
+    })
+}
+
+function submitFormulario(e){
+    e.preventDefault();
 }
